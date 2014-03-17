1c490ec17cb80ce57ed208bfb2cb36b4837b704d
Peter Zijlstra <peterz@infradead.org>
2014-03-17 18:06:10 +0100

    arch: Mass conversion of smp_mb__*()
    Mostly scripted conversion of the smp_mb__* barriers.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/n/tip-55dhyhocezdw1dg7u19hmh1u@git.kernel.org
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-arch@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: 4e857c58efeb99393cba5a5d0d8ec7117183137c
    [joonwoop@codeaurora.org: fixed trivial merge conflict.]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2675,7 +2675,7 @@ static inline bool __must_check current_set_polling_and_test(void)
 	 *
 	 * XXX: assumes set/clear bit are identical barrier wise.
 	 */
-	smp_mb__after_clear_bit();
+	smp_mb__after_atomic();
 
 	return unlikely(tif_need_resched());
 }
