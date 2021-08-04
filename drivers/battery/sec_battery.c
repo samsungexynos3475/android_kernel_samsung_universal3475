fe79d79a022d4eba861eab4b813b5ad7bb8cb367
ivanmeler <i_ivan@windowslive.com>
2021-08-04 08:42:15 +0000

    sec_battery.c: Adjust storemode values for optimal battery life
    https://batteryuniversity.com/article/bu-808-how-to-prolong-lithium-based-batteries
    75/25 looks to be middle ground
    
    Change-Id: Ic930682d745e216cb2630f08242504ed44362682

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -13,8 +13,8 @@
 
 bool slate_mode_state;
 
-static unsigned int STORE_MODE_CHARGING_MAX = 90;
-static unsigned int STORE_MODE_CHARGING_MIN = 20;
+static unsigned int STORE_MODE_CHARGING_MAX = 75;
+static unsigned int STORE_MODE_CHARGING_MIN = 25;
 
 module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, uint, S_IWUSR | S_IRUGO);
 module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, uint, S_IWUSR | S_IRUGO);
