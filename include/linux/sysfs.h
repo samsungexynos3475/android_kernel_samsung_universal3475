c35aa9c14bb44c7447bc008797746c6f880afb2e
lavenly <135024283+lavenly@users.noreply.github.com>
2026-06-13 07:04:44 -0700

    zram: backport modern sysfs __ATTR macros
    Adds __ATTR_RO, __ATTR_WO, __ATTR_RW and DEVICE_ATTR counterparts to legacy 3.10 headers to resolve 'undeclared' compiler errors when building the backported 3.18 ZRAM driver.
    
    Change-Id: I6f881d4e0e204f79df8168b24d8e1ebfb0334433

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -75,9 +75,17 @@ struct attribute_group {
 }
 
 #define __ATTR_RO(_name) { \
-	.attr	= { .name = __stringify(_name), .mode = 0444 },	\
-	.show	= _name##_show,					\
+        .attr   = { .name = __stringify(_name), .mode = 0444 },      \
+        .show   = _name##_show,                                         \
 }
+
+#define __ATTR_WO(_name) { \
+        .attr   = { .name = __stringify(_name), .mode = 0200 },      \
+        .store  = _name##_store,                                        \
+}
+
+#define __ATTR_RW(_name) __ATTR(_name, 0644, \
+                         _name##_show, _name##_store)
 
 #define __ATTR_NULL { .attr = { .name = NULL } }
 
