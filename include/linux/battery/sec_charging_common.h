88c59dc3dd12e8b47c278e7609970366ce27c0a9
lavenly <135024283+lavenly@users.noreply.github.com>
2026-06-16 17:47:07 -0700

    kernel: drivers: Silence minor battery and touchscreen dmesg spam
    Change-Id: I7e6fd648959ab4572d45331733fb62143f1796c7

--- include/linux/battery/sec_charging_common.h
+++ include/linux/battery/sec_charging_common.h
@@ -737,13 +737,13 @@ static inline struct power_supply *get_power_supply_by_name(char *name)
 	int ret;	\
 	psy = get_power_supply_by_name((name));	\
 	if (!psy) {	\
-		pr_err("%s: Fail to "#function" psy (%s)\n",	\
+		pr_debug("%s: Fail to "#function" psy (%s)\n",	\
 			__func__, (name));	\
 		value.intval = 0;	\
 	} else {	\
 		ret = psy->function##_property(psy, (property), &(value)); \
 		if (ret < 0) {	\
-			pr_err("%s: Fail to "#name" "#function" (%d=>%d)\n", \
+			pr_debug("%s: Fail to "#name" "#function" (%d=>%d)\n", \
 				__func__, (property), ret);	\
 			value.intval = 0;	\
 		}	\
